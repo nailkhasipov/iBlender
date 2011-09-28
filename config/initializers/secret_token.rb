84d61fadf868cb4a6ace4d4199b101e9abac09a4
jonygreen <jonygreen@gmail.com>
2011-09-29 00:44:40 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iblender::Application.config.secret_token = '7f8ccaeb42b3117afc914fb2d18911f0b32bafe7ef43c0a6448d7afcde2e46ecf6094658c7f9a4b6ff7a5ce154eed3ed60cf07f7b96392a71722f3647953fd22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Iblender::Application.config.session_store :cookie_store, :key => '_iblender_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Iblender::Application.config.session_store :active_record_store
